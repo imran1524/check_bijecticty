165cffec6a58bf044122f43f3ee34b8709595ae8
imran1524 <imran1524@gmail.com>
2023-04-25 13:24:32 +0100

    Calculate #Xv(j)

--- main.c
+++ main.c
@@ -5,28 +5,29 @@
 int i = 256;
 int j = 8;
 int** temp;
+
 // Function to generate a random binary value (0 or 1)
 int generateBinaryValue() {
     return rand() % 2;
 }
-int **v_i_j;
+int**v_i_j;
 int** allocate_2D_matrix(int rows, int cols);
 int** generateSboxMatrix(int rows, int cols);
 int** generate_WHM(int k);
 void free_2D_array(int** array_2D, int array_2D_length);
+int* calculate_x_v(int* A, int n);
 
 int main() {
-
-    int** ptr_sbox_matrix = allocate_2D_matrix(i, j);
+    int **ptr_sbox_matrix = allocate_2D_matrix(i, j);
     ptr_sbox_matrix = generateSboxMatrix(i, j);
 
     // Print the generated matrix
     printf("Generated Matrix:\n");
     for (int rows = 0; rows < i; rows++) {
         for (int cols = 0; cols < j; cols++) {
-            printf("%d",  ptr_sbox_matrix[rows][cols]);
+//            printf("%d",  ptr_sbox_matrix[rows][cols]);
         }
-        printf("\n");
+//        printf("\n");
     }
 
     //j = 0
@@ -54,67 +55,65 @@ int main() {
     //j = 7
     //v_0_7 = (a_0_7 * 2^7) + (a_0_6 * 2^6) + (a_0_5 + 2^5) + (a_0_4 + 2^4) + (a_0_3 + 2^3) + (a_0_2 + 2^2) + (a_0_1 + 2^1) + (a_0_0 + 2^0)
     //v_1_7 = (a_1_7 * 2^7) + (a_1_6 * 2^6) + (a_1_5 + 2^5) + (a_1_4 + 2^4) + (a_1_3 + 2^3) + (a_1_2 + 2^2) + (a_1_1 + 2^1) + (a_1_0 + 2^0)
+
     //...
     //v_254_7 = (a_254_7 * 2^7) + (a_254_6 * 2^6) + (a_254_5 + 2^5) + (a_254_4 + 2^4) + (a_254_3 + 2^3) + (a_254_2 + 2^2) + (a_254_1 + 2^1) + (a_254_0 + 2^0)
     //v_255_7 = (a_255_7 * 2^7) + (a_255_6 * 2^6) + (a_255_5 + 2^5) + (a_255_4 + 2^4) + (a_255_3 + 2^3) + (a_255_2 + 2^2) + (a_255_1 + 2^1) + (a_255_0 + 2^0)
 
-    // X_v_j
-    // int* X_v = (int*) malloc(i * sizeof(int));
     int offset = 0;
-    v_i_j = allocate_2D_matrix(i,j);
-    int sum = 0;
-    for(int rows = 0; rows < i; rows++){
+    v_i_j = allocate_2D_matrix(i, j);
+    for (int rows = 0; rows < i; rows++) {
         offset = 1;
         int sum = 0;
-        for(int cols = j-1; cols >=0; cols--){
-            sum = ptr_sbox_matrix[rows][cols] * 1 << offset-1;
-            v_i_j[rows][offset-1] = sum;
+        for (int cols = j - 1; cols >= 0; cols--) {
+            sum = ptr_sbox_matrix[rows][cols] * 1 << offset - 1;
+            v_i_j[rows][offset - 1] = sum;
             offset++;
         }
     }
 
-    printf("cols = 0\n");
-    for(int cols = 0; cols  < 1; cols++){
-        for(int rows = 0; rows < 256; rows++){
-            printf("v_i_j[%d][%d] = %d\n", cols, rows, v_i_j[rows][cols]);
+    //PRINTING THE v_i_j
+    for (int cols = 1; cols < j; cols++) {
+        for (int rows = 0; rows < i; rows++) {
+            v_i_j[rows][cols] = v_i_j[rows][cols] + v_i_j[rows][cols - 1];
         }
     }
 
-    //PRINTING THE WEIGHTED VALUES OF THE BITS
-    printf("v_i_j[%d][%d] = %d\n", 0, 0, v_i_j[0][0]);
-    for(int cols = 1; cols < j; cols++){
-        printf("cols = %d\n", cols);
-        for(int rows = 0; rows < i; rows++){
-            v_i_j[rows][cols] = v_i_j[rows][cols] + v_i_j[rows][cols-1];
-            printf("v_i_j[%d][%d] = %d\n", rows, cols, v_i_j[rows][cols]);
+    int *temp = (int *) malloc(j * sizeof(int));
+    int *x_v = (int *) malloc(i * sizeof(int));
+
+    for (int cols = 0; cols < j; cols++) {
+        printf("j = %d\n", cols);
+        for (int rows = 0; rows < i; rows++) {
+            temp[rows] = v_i_j[rows][cols];
         }
+
+        int sum = 0;
+
+       x_v = calculate_x_v(temp, i);
+        for (int index = 0; index < i; index++) {
+            printf("x_v[%d] = %d\n", index, x_v[index]);
+            sum = x_v[index] + sum;
+        }
+        printf("%d\n", sum);
+        printf("\n");
     }
 
-
-//
-//    for(int cols = 0; cols < j; cols++){
-//        printf("j = %d\n", cols);
-//        for(int rows = 0; rows < i; rows++){
-//            printf("v_i_j[%d][%d] = %d\n", rows, cols, v_i_j[rows][cols]);
-//        }
-//    }
-
-    //ACCUMULATIONS
-    //CALCULATE BIJECTIVE FITNESS FUNCTION
-    //Free S-box
     free_2D_array(ptr_sbox_matrix, i);
-
+    free_2D_array(v_i_j, i);
+    free(temp);
+    free(x_v);
     return 0;
-} //End of mail connection
+} //END
 
-
+//-----------------------------------------------------------------------------------------------------------
+//DECLARATION OF FUNCTIONS
 //FUNCTION DEFINITION OF 2D ALLOCATION
 int** allocate_2D_matrix(int rows, int cols){
     int** array_2D = (int**) malloc(rows * sizeof(int));
     for(int index = 0; index < rows; index++){
         array_2D[index] = (int*) malloc(cols * sizeof(int));
     }
-
     return array_2D;
 }
 
@@ -139,7 +138,6 @@ int** generateSboxMatrix(int row, int col){
         }
     }
     return matrix;
-
 }
 //CALCULATE WALSH HADAMARD MATRIX
 //Function definition of generating Walsh Hadamard Matrix of order k
@@ -165,14 +163,6 @@ int** generate_WHM(int k) {
         }
     }
 
-    // Displaying the final hadamard matrix
-//    for (int i = 0; i < 256; i++) {
-//        for (int j = 0; j < 256; j++) {
-//            printf(" %d ",hadamard[i][j]);
-//        }
-//        printf("\n");
-//    }
-
     return hadamard;
 }
 
@@ -187,3 +177,28 @@ void free_2D_array(int** array_2D, int array_2D_length){
     free(array_2D);
 }
 
+// Function to calculate the frequency of all elements in an array
+int* calculate_x_v(int* A, int n){
+    // create a count array of size `n` to store the count of all array elements
+    int freq[n];
+    int* x_v = (int*)malloc(n * sizeof(int));
+    for (int i = 0; i < n; i++) {
+        freq[i] = 0;
+    }
+
+    // update frequency of each element
+    for (int i = 0; i < n; i++) {
+        freq[A[i]]++;
+    }
+    int counter = 0;
+    // iterate through the array to print frequencies
+    for (int i = 0; i < n; i++)
+    {
+        if (freq[i]) {
+            //printf("%d: %d\n", i, freq[i]);
+            x_v[i] = freq[i];
+            counter++;
+        }
+    }
+    return x_v;
+}
