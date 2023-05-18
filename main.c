698b9959987bdcd0037d0255af9c5cbaee137cb5
imran1524 <imran1524@gmail.com>
2023-05-18 09:46:16 +0100

    Calculation of step 3 with W+_1,j, W-_2,j, W+_2,j and W-_2,j

--- main.c
+++ main.c
@@ -16,18 +16,31 @@ int i;
 int j;
 int** v_i_j;
 int* v_j;
-struct W_j_object W_j_1_plus;
-struct W_j_object W_j_1_minus;
-struct W_j_object W_j_2_plus;
-struct W_j_object W_j_2_minus;
-struct W_j_object W_j_3_plus;
-struct W_j_object W_j_3_minus;
+//struct W_j_object W_j_1_plus;
+//struct W_j_object W_j_1_minus;
+//struct W_j_object W_j_2_plus;
+//struct W_j_object W_j_2_minus;
+//struct W_j_object W_j_3_plus;
+//struct W_j_object W_j_3_minus;
+//
+//struct array_object W_j_1;
+//struct array_object W_j_1_2_plus;
+//struct array_object W_j_1_2_minus;
+//struct array_object W_j_2_3_plus;
+//struct array_object W_j_2_3_minus;
+
+struct W_j_object W_plus_j_1;
+struct W_j_object W_minus_j_1;
+struct W_j_object W_plus_j_2;
+struct W_j_object W_minus_j_2;
+struct W_j_object W_plus_j_3;
+struct W_j_object W_minus_j_3;
 
 struct array_object W_j_1;
-struct array_object W_j_1_2_plus;
-struct array_object W_j_1_2_minus;
-struct array_object W_j_2_3_plus;
-struct array_object W_j_2_3_minus;
+struct array_object W_plus_j_1_2;
+struct array_object W_minus_j_1_2;
+struct array_object W_plus_j_2_3;
+struct array_object W_minus_j_2_3;
 
 //FUNCTION SIGNATURES
 int generateBinaryValue();
@@ -39,12 +52,12 @@ int* calculate_X_v(int* A, int n);
 int calculate_F_D_j(int *x_k_j, int rows, int cols);
 int** generate_WHM(int order);
 int calculate_WH_max (int* walsh_spectrum, int size);
-struct W_j_object calculate_W_1_plus(int* F_omaga, int size, int WH_max);
-struct W_j_object calculate_W_1_minus(int* F_omega, int size, int WH_max);
-struct W_j_object calculate_W_2_plus(int* F_omega, int size, int WH_max);
-struct W_j_object calculate_W_2_minus(int* F_omega, int size, int WH_max);
-struct W_j_object calculate_W_3_plus(int* F_omega, int size, int WH_max);
-struct W_j_object calculate_W_3_minus(int* F_omega, int size, int WH_max);
+struct W_j_object calculate_W_plus_1(int* F_omaga, int size, int WH_max);
+struct W_j_object calculate_W_minus_1(int* F_omega, int size, int WH_max);
+struct W_j_object calculate_W_plus_2(int* F_omega, int size, int WH_max);
+struct W_j_object calculate_W_minus_2(int* F_omega, int size, int WH_max);
+struct W_j_object calculate_W_plus_3(int* F_omega, int size, int WH_max);
+struct W_j_object calculate_W_minus_3(int* F_omega, int size, int WH_max);
 int* matrix_multiplication(int *array, int **matrix);
 struct array_object calculate_union(int* array_1, int* array_2, int array_size_1, int array_size_2);
 int complement_f_j_i(int f_j_i);
@@ -116,15 +129,16 @@ int main() {
         //printf("\n");
 
         //CALCULATION OF V
+        //STEP # 1: FOR D(j) construct a set V = {v = v| v ∈ {0, 1, 2, 3, ..., 2^(j+1)-1} if #{X_v(j) > 2^n-1-j}
         int v = (1 << (col + 1)) - 1;
-        printf("v = %d\n", v);
-        int index_v = 0;
+        //printf("v = %d\n", v);
+        int index_V = 0;
         for (int index = 0; index <= v; index++) {
             //printf("#{X_v[%d]} = %d\n", index, X_v[index]);
             //printf("2^n-1-%d  = %d\n", col, 1 << (n - 1 - col));
 
             if (X_v[index] > (1 << (n - 1 - col))) {
-                V[index_v++] = index;
+                V[index_V++] = index;
                 //printf("%d > %d\n", X_v[index], 1 << (n - 1 - col));
                 //printf("v = %d\n", index);
             }
@@ -132,43 +146,46 @@ int main() {
             //sum = X_v[index] + sum;
         }
 
-        //printf("index_v = %d\n");
-        for (int index = 0; index < index_v; index++) {
-            printf("V[%d] = %d\n", index, V[index]);
+        //printf("index_V = %d\n");
+        for (int index = 0; index < index_V; index++) {
+            //printf("V[%d] = %d\n", index, V[index]);
         }
 
 
-        //APPLYING WALSH HADAMARD MATRIX ON f_j TO CALCULATE WALSH HADAMARD SPECTRUM OF f_j
-        S_j_omega = matrix_multiplication(f, WH);
-        F_omega = S_j_omega;
+//STEP # 2: IF V IS ∅, OUTPUT D(j), OTHERWISE GO TO STEP # 3
+        if(index_V == 0){
+            for(int row = 0; row < i; row++){
+                printf("D[%d][%d] = %d\n", row, col, D[row][col]);
+            }
+        }else{
 
-        // printf("j = %d\n", col);
-        //printf("Walsh-Hadamard transform for f Boolean function:\n");
-        for (int omega = 0; omega < i; omega++) {
-            //printf("%d ", S_j_omega[omega]);
-            //printf("f[%d] = %d\n", omega,  f[omega]);
-            //printf("S_j_omega[%d] = %d\n", omega, S_j_omega[omega]);
-            //printf("f[%d] = %d\n", omega,  F_omega[omega]);
+            //STEP # 3. According to the Boolean function f_j in D(j), calculate the sets W^plus_j,1, W^minus_j,1,W^plus_j,2 AND W^minus_j,2.
+            //APPLYING WALSH HADAMARD MATRIX ON f_j TO CALCULATE WALSH HADAMARD SPECTRUM OF f_j
+            S_j_omega = matrix_multiplication(f, WH);
+
+            // printf("j = %d\n", col);
+            //printf("Walsh-Hadamard transform for f Boolean function:\n");
+            for (int omega = 0; omega < i; omega++) {
+                //printf("%d ", S_j_omega[omega]);
+                //printf("f[%d] = %d\n", omega,  f[omega]);
+                //printf("S_j_omega[%d] = %d\n", omega, S_j_omega[omega]);
+                //printf("f[%d] = %d\n", omega,  F_omega[omega]);
+            }
+            WH_max = calculate_WH_max(S_j_omega, i);
+            printf("WH_max = %d\n", WH_max);
+
+            W_plus_j_1 = calculate_W_plus_1(S_j_omega, i, WH_max);
+            W_minus_j_1 = calculate_W_minus_1(S_j_omega, i, WH_max);
+            W_plus_j_2 = calculate_W_plus_2(S_j_omega, i, WH_max);
+            W_minus_j_2 = calculate_W_minus_2(S_j_omega, i, WH_max);
+            W_plus_j_3 = calculate_W_plus_3(S_j_omega, i, WH_max);
+            W_minus_j_3 = calculate_W_minus_3(S_j_omega, i, WH_max);
+
+            printf("\n");
         }
-        WH_max = calculate_WH_max(S_j_omega, i);
-        //printf("WH_max = %d\n", WH_max);
-
-        W_j_1_plus = calculate_W_1_plus(S_j_omega, i, WH_max);
-        W_j_1_minus = calculate_W_1_minus(S_j_omega, i, WH_max);
-        W_j_2_plus = calculate_W_2_plus(S_j_omega, i, WH_max);
-        W_j_2_minus = calculate_W_2_minus(S_j_omega, i, WH_max);
-        W_j_3_plus = calculate_W_3_plus(S_j_omega, i, WH_max);
-        W_j_3_minus = calculate_W_3_minus(S_j_omega, i, WH_max);
-        printf("\n");
     }
 
 #if 0
-    {
-        //printf("index_v = %d\n", index_v);
-        //CALCULATING BIJECTIVE FITNESS FUNCTION, F(D(j))
-        F_D_j[col] = calculate_F_D_j(X_v, j, col);
-        //printf("F_D_j[%d] = %d\n", col, F_D_j[col]);
-        //printf("\n");
 #if 1
         //STEP 5: Set a=V(m),here V(m)is the m-th element in V int a;
         int a;
@@ -426,20 +443,20 @@ int main() {
     free(V);
     V = NULL;
 
-    free(W_j_1_plus.array);
-    W_j_1_plus.array = NULL;
+    free(W_plus_j_1.array);
+    W_plus_j_1.array = NULL;
 
-    free(W_j_1_minus.array);
-    W_j_1_plus.array = NULL;
+    free(W_minus_j_1.array);
+    W_plus_j_1.array = NULL;
 
-    free(W_j_2_plus.array);
-    W_j_2_plus.array = NULL;
+    free(W_plus_j_2.array);
+    W_plus_j_2.array = NULL;
 
-    free(W_j_2_minus.array);
-    W_j_2_minus.array = NULL;
+    free(W_minus_j_2.array);
+    W_minus_j_2.array = NULL;
 
-    free(W_j_3_plus.array);
-    W_j_3_plus.array = NULL;
+    free(W_plus_j_3.array);
+    W_plus_j_3.array = NULL;
 
     free(X_v);
     X_v = NULL;
@@ -577,127 +594,127 @@ int** generate_WHM(int order){
     return hadamard;
 }
 
-struct W_j_object calculate_W_1_plus(int* F_omaga, int size, int WH_max){
-    struct W_j_object W_1_plus;
+struct W_j_object calculate_W_plus_1(int* F_omaga, int size, int WH_max){
+    struct W_j_object W_plus_1;
     int index = 0;
     int counter = 0;
-    W_1_plus.array = (int*)malloc(i * sizeof(int));
-    W_1_plus.array_size = 0;
+    W_plus_1.array = (int*)malloc(i * sizeof(int));
+    W_plus_1.array_size = 0;
 //    printf("WH_max = %d\n", WH_max);
     for(int omega = 0; omega < size; omega++){
         if (F_omaga[omega] == WH_max) {
-            W_1_plus.array[index++] = omega;
+            W_plus_1.array[index++] = omega;
             counter++;
         }
     }
-    W_1_plus.array_size = counter;
-
-    for(int omega = 0; omega < W_1_plus.array_size; omega++){
-//        printf("W_1_plus[%d] = %d:\n", index, W_1_plus.array[omega]);
+    W_plus_1.array_size = counter;
+    printf("W_plus_1:\n");
+    for(int omega = 0; omega < W_plus_1.array_size; omega++){
+        printf("W_plus_1[%d] = %d:\n", index, W_plus_1.array[omega]);
     }
-    return W_1_plus;
+    return W_plus_1;
 }
 
 //w_1_2_plus
-struct W_j_object calculate_W_1_minus(int* F_omaga, int size, int WH_max){
+struct W_j_object calculate_W_minus_1(int* F_omaga, int size, int WH_max){
     int index = 0;
     int counter = 0;
-    struct W_j_object W_1_minus;
-    W_1_minus.array = (int*)malloc(i * sizeof(int));
+    struct W_j_object W_minus_1;
+    W_minus_1.array = (int*)malloc(i * sizeof(int));
 //    printf("-WH_max = %d\n", -WH_max);
     for(int omega = 0; omega < size; omega++){
         if (F_omaga[omega] == -WH_max) {
-            W_1_minus.array[index++] = omega;
+            W_minus_1.array[index++] = omega;
             counter++;
         }
     }
-    W_1_minus.array_size = counter;
+    W_minus_1.array_size = counter;
 
-    for(int omega = 0; omega < W_1_minus.array_size; omega++){
-//        printf("W_1_minus[%d] = %d:\n", omega, W_1_minus.array[omega]);
+    for(int omega = 0; omega < W_minus_1.array_size; omega++){
+        printf("W_minus_1[%d] = %d:\n", omega, W_minus_1.array[omega]);
     }
-    return W_1_minus;
+    return W_minus_1;
 }
 
 //w_2_plus
-struct W_j_object calculate_W_2_plus(int* F_omega, int size, int WH_max){
+struct W_j_object calculate_W_plus_2(int* F_omega, int size, int WH_max){
     int index = 0;
     int counter = 0;
-    struct W_j_object W_2_plus;
-    W_2_plus.array = (int*) malloc(i * sizeof(int));
+    struct W_j_object W_plus_2;
+    W_plus_2.array = (int*) malloc(i * sizeof(int));
 //    printf("WH_max - 2 = %d\n", WH_max - 2);
     for(int omega = 0; omega < size; omega++){
         if(F_omega[omega] == WH_max - 2){
-            W_2_plus.array[index++] = omega;
+            W_plus_2.array[index++] = omega;
             counter++;
         }
     }
-    W_2_plus.array_size = counter;
+    W_plus_2.array_size = counter;
 
-    for(int omega = 0; omega < W_2_plus.array_size; omega++){
-//        printf("W_2_plus[%d] = %d:\n", omega, W_2_plus.array[omega]);
+    for(int omega = 0; omega < W_plus_2.array_size; omega++){
+        printf("W_plus_2[%d] = %d:\n", omega, W_plus_2.array[omega]);
     }
-    return  W_2_plus;
+    return  W_plus_2;
 }
 
 //w_2_minus
-struct W_j_object calculate_W_2_minus(int* F_omega, int size, int WH_max){
+struct W_j_object calculate_W_minus_2(int* F_omega, int size, int WH_max){
     int index = 0;
     int counter = 0;
-    struct W_j_object W_2_minus;
+    struct W_j_object W_minus_2;
 //    printf("-WH_max + 2 = %d\n", -WH_max + 2);
-    W_2_minus.array = (int*)malloc(i * sizeof(int));
+    W_minus_2.array = (int*)malloc(i * sizeof(int));
     for(int omega = 0; omega < size; omega++){
         if(F_omega[omega] == -WH_max + 2){
-            W_2_minus.array[index++] = omega;
+            W_minus_2.array[index++] = omega;
             counter++;
         }
     }
-    W_2_minus.array_size = counter;
-    for(int omega = 0; omega < W_2_minus.array_size; omega++){
-//        printf("W_2_minus[%d] = %d:\n", omega, W_2_minus.array[omega]);
+    W_minus_2.array_size = counter;
+    for(int omega = 0; omega < W_minus_2.array_size; omega++){
+        printf("W_minus_2[%d] = %d:\n", omega, W_minus_2.array[omega]);
     }
-    return W_2_minus;
+    return W_minus_2;
 }
 
 //w_3_plus
-struct W_j_object calculate_W_3_plus(int* F_omega, int size, int WH_max){
+struct W_j_object calculate_W_plus_3(int* F_omega, int size, int WH_max){
     int index = 0;
     int counter = 0;
-    struct W_j_object W_3_plus;
+    struct W_j_object W_plus_3;
 //    printf("WH_max - 4 = %d\n", WH_max - 4);
-    W_3_plus.array = (int*)malloc(i * sizeof(int));
+    W_plus_3.array = (int*)malloc(i * sizeof(int));
     for(int omega = 0; omega < size; omega++) {
         if (F_omega[omega] == WH_max - 4) {
-            W_3_plus.array[index++] = omega;
+            W_plus_3.array[index++] = omega;
             counter++;
         }
     }
-    W_3_plus.array_size = counter;
-    for(int omega = 0; omega < W_3_plus.array_size; omega++){
-//        printf("W_3_plus[%d] = %d\n", omega, W_3_plus.array[omega]);
+    W_plus_3.array_size = counter;
+    for(int omega = 0; omega < W_plus_3.array_size; omega++){
+        printf("W_plus_3[%d] = %d\n", omega, W_plus_3.array[omega]);
     }
-    return W_3_plus;
+    return W_plus_3;
 }
 
 //w_3_minus
-struct W_j_object calculate_W_3_minus(int* F_omega, int size, int WH_max){
+struct W_j_object calculate_W_minus_3(int* F_omega, int size, int WH_max){
     int index = 0;
     int counter = 0;
-    struct W_j_object W_3_minus;
+    struct W_j_object W_minus_3;
 //    printf("-WH_max + 4 = %d\n", -WH_max + 4);
-    W_3_minus.array = (int*) malloc(i * sizeof(int));
+    W_minus_3.array = (int*) malloc(i * sizeof(int));
     for(int omega = 0; omega < size; omega++){
         if(F_omega[omega] == (-WH_max) + 4){
-            W_3_minus.array[index++] = omega;
+            W_minus_3.array[index++] = omega;
             counter++;
         }
     }
-    W_3_minus.array_size = counter;
-    for(int omega = 0; omega < W_3_minus.array_size; omega++){
-//        printf("W_3_minus[%d] = %d\n", omega, W_3_minus.array[omega]);
+    W_minus_3.array_size = counter;
+    for(int omega = 0; omega < W_minus_3.array_size; omega++){
+        printf("W_minus_3[%d] = %d\n", omega, W_minus_3.array[omega]);
     }
-    return W_3_minus;
+    return W_minus_3;
 }
 
 int* matrix_multiplication(int *array, int **matrix) {
@@ -797,7 +814,6 @@ int** calculate_v_i_j(int** S_box, int row_number, int col_number) {
         }
     }
 
-
     //CALCULATING INTEGER VALUE OF v_i_j FOR EACH ROW OF D(j) when j = 1, 2, 3, ... n-1
     //FOR EXAMPLE, j = 2: (v[0][1] * 2^1) + (v[0][0] * 2^0)
     //             j = 7: (v[0][7] * 2^7) +
@@ -824,6 +840,5 @@ int** calculate_v_i_j(int** S_box, int row_number, int col_number) {
             //printf("v_j[%d] = %d\n", row, v_j[row]);
         }
     }
-
     return v_i_j;
 }
